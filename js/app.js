54c3b9184e96f5b2deddc29e6bcfc9a30395d349
Erin Johnson <erinjohnson47@gmail.com>
2019-10-18 16:26:57 -0700

    copy edit of about me, check home link to about, comment out project 2 placeholder code

--- js/app.js
+++ js/app.js
@@ -1,2 +1,23 @@
 console.log('This JS file has loaded.')
 
+const projectParent = document.getElementById('projects')
+
+const projectTitle1 = document.getElementById('project-title-1')
+
+const projectTitle2 = document.getElementById('project-title-2')
+
+const projectImage1 = document.getElementById('project-image-1')
+
+const projectOneLiner = document.getElementById('project-one-liner')
+
+const projectDetails = document.getElementById('project-details')
+
+const projectTech = document.getElementById('project-tech')
+
+//figure out if I can change EXISTING data first
+//then figure out how to map 
+
+// const projectShell = document.getElementById('project-shell')
+
+// const newJumbotron = projectShell.cloneNode(true);
+
