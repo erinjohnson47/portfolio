45375aedb95c20a40ff8df9d967b9173f62caaca
Erin Johnson <erinjohnson47@gmail.com>
2019-10-21 19:04:08 -0700

    fix more about/less about me buttons

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
 console.log('This JS file has loaded.')
 
-const projectParent = document.getElementById('projects')
+// const projectParent = document.getElementById('projects')
 
-const projectTitle1 = document.getElementById('project-title-1')
+// const projectTitle1 = document.getElementById('project-title-1')
 
-const projectTitle2 = document.getElementById('project-title-2')
+// const projectTitle2 = document.getElementById('project-title-2')
 
-const projectImage1 = document.getElementById('project-image-1')
+// const projectImage1 = document.getElementById('project-image-1')
 
-const projectOneLiner = document.getElementById('project-one-liner')
+// const projectOneLiner = document.getElementById('project-one-liner')
 
-const projectDetails = document.getElementById('project-details')
+// const projectDetails = document.getElementById('project-details')
 
-const projectTech = document.getElementById('project-tech')
+// const projectTech = document.getElementById('project-tech')
 
 //figure out if I can change EXISTING data first
 //then figure out how to map 
@@ -22,7 +22,16 @@ const projectTech = document.getElementById('project-tech')
 // const newJumbotron = projectShell.cloneNode(true);
 
 const moreAboutMe = document.getElementById('see-more');
-const ariaAtrribute = moreAboutMe.getAttribute('aria-expanded');
+const lessAboutMe = document.getElementById('see-less')
+const ariaExpandMore = moreAboutMe.getAttributeNode('aria-expanded').value;
+const ariaExpandLess = lessAboutMe.getAttributeNode('aria-expanded').value;
 
-moreAboutMe.addEventListener("onclick", () =>  ariaAtrribute ? moreAboutMe.innerText = "... less about me" : moreAboutMe.innerText = "more about me ...");
-console.log(moreAboutMe)
+moreAboutMe.addEventListener("click", () => {
+    moreAboutMe.style.display = "none";
+    lessAboutMe.style.display = 'block'
+}) 
+
+lessAboutMe.addEventListener('click', () => {
+    moreAboutMe.style.display = "block"
+    lessAboutMe.style.display = 'none'
+} )
